b7daf0ccb525e80fb1ecab7bc3c8114030fec18c
vijayvigneshselvaraj <vijayvigneshselvaraj@gmail.com>
2020-04-07 17:14:31 +0200

    Created a Constants file to keep the constants

---
+++ src/main/java/com/vijay/kata/Constants.java
@@ -0,0 +1,7 @@
+package com.vijay.kata;
+
+public class Constants {
+    public static final int MAX_BOWLING_TURN = 10;
+    public static final int STRIKE_SCORE = 30;
+    public static final int SPARE_SCORE = 10;
+}
--- src/main/java/com/vijay/kata/ScoreBoard.java
+++ src/main/java/com/vijay/kata/ScoreBoard.java
@@ -6,19 +6,18 @@ public class ScoreBoard {
 
     public int getBowlingScore(ArrayList<Frame> frames) {
         int score = 0;
-        int max_turns = 10;
         Frame eachFrame;
-        for(int eachRawFrameValue=0; eachRawFrameValue < max_turns; eachRawFrameValue++) {
+        for(int eachRawFrameValue=0; eachRawFrameValue < Constants.MAX_BOWLING_TURN; eachRawFrameValue++) {
             eachFrame = frames.get(eachRawFrameValue);
             if(isStrike(eachFrame)) {
-                score = 30 + score;
+                score = Constants.STRIKE_SCORE + score;
             }
             if(isMiss(eachFrame)) {
                 int eachFrameScore = Integer.parseInt(eachFrame.getFrameValue().replace("-", ""));
                 score = eachFrameScore + score;
             }
             if(isSpare(eachFrame)) {
-                score = calculateCurrentFrameScore(eachFrame).getScore() + score + 10;
+                score = calculateCurrentFrameScore(eachFrame).getScore() + score + Constants.SPARE_SCORE;
             }
         }
         return score;
@@ -38,7 +37,7 @@ public class ScoreBoard {
 
     private Frame calculateCurrentFrameScore(Frame frame) {
         int eachFrameScore = 0;
-        if(frame.getFrameId() == 10) {
+        if(frame.getFrameId() == Constants.MAX_BOWLING_TURN) {
             eachFrameScore = Integer.parseInt(frame.getFrameValue().replace("/", ""));
             eachFrameScore = eachFrameScore%10;
             frame.setScore(eachFrameScore);
