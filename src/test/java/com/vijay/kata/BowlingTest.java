8f044561ead2a498e59b5dabf595c23b8f2e2aae
vijayvigneshselvaraj <vijayvigneshselvaraj@gmail.com>
2020-04-07 13:45:31 +0200

    Added a testcase to split frames for given X as roll sequence

--- src/test/java/com/vijay/kata/BowlingTest.java
+++ src/test/java/com/vijay/kata/BowlingTest.java
@@ -1,10 +1,17 @@
 package com.vijay.kata;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BowlingTest {
+    public Bowling b = null;
+
+    @Before
+    public void setup() {
+        b = new Bowling();
+    }
 
     @Test
     public void baseTest() {
@@ -13,12 +20,19 @@ public class BowlingTest {
 
     @Test
     public void parseInputSequenceOfRollShouldReturnNullWhenEmptyInputRollSequencePassedTest() {
-        assertNull(new Bowling().parseInputSequenceOfRoll(""));
+        assertNull(b.parseInputSequenceOfRoll(""));
     }
 
     @Test
     public void parseInputSequenceOfRollShouldReturnNullWhenNullInputRollSequencePassedTest() {
-        assertNull(new Bowling().parseInputSequenceOfRoll(null));
+        assertNull(b.parseInputSequenceOfRoll(null));
+    }
+
+    @Test
+    public void parseInputSequenceOfRollShouldReturnNullWhenXGivenInputRollSequencePassedTest() {
+        String rollSequence = "X X X X";
+        String[] expectedParsedRollSequence = new String[]{"X", "X", "X", "X"};
+        assertEquals(b.parseInputSequenceOfRoll(rollSequence), expectedParsedRollSequence);
     }
 
 }
