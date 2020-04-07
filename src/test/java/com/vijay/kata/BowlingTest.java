2d036e914b14aa59ecf08acb74d3bdf0f15e8509
vijayvigneshselvaraj <vijayvigneshselvaraj@gmail.com>
2020-04-07 13:00:24 +0200

    Added a basic test case and updated the pom to support junit

--- src/test/java/com/vijay/kata/BowlingTest.java
+++ src/test/java/com/vijay/kata/BowlingTest.java
@@ -1,5 +1,13 @@
 package com.vijay.kata;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class BowlingTest {
 
+    @Test
+    public void baseTest() {
+        assertTrue(true);
+    }
 }
