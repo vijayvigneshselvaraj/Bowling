11b9912f0990fa9c02b03d3c4fb5f13b05536b61
vijayvigneshselvaraj <vijayvigneshselvaraj@gmail.com>
2020-04-08 14:59:42 +0200

    Corrected the BowlingRunner code to support more than one frame

--- src/main/java/com/vijay/kata/BowlingRunner.java
+++ src/main/java/com/vijay/kata/BowlingRunner.java
@@ -3,8 +3,13 @@ package com.vijay.kata;
 public class BowlingRunner {
     public static void main(String[] args) {
         Bowling b = new Bowling();
-        if (args.length==1)
-            System.out.println(b.getScore(args[0]));
+        String scoreSequence = "";
+        if (args.length>=1) {
+            for(String arg : args) {
+                scoreSequence = scoreSequence + " " + arg;
+            }
+            System.out.println(b.getScore(scoreSequence));
+        }
         else
             System.out.println("Required: 1 str arg. Found: Invalid no of args / no args ! ! !");
     }
